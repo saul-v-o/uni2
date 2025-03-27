d6e2888698981b5e147284838e96d8c8e16c9648
Saul <saul_valenzuela_31@hotmail.com>
2025-03-27 09:23:33 -0700

    Tercer commit: Creación de Tabla con MUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,37 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"
 
 function App() {
-  const [count, setCount] = useState(0)
-  useEffect(()=>{
-    if (count > 10) {
-      setCount(10)
-      alert("Llegaste al límite del contador")
-    }
-  },[count])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const usersList = [
+    {name:"Juan", lastName:"Mendoza", age:17}, 
+    {name:"Luis", lastName:"Pérez", age:16}, 
+    {name:"Jorge", lastName:"Fernández", age:21}]
+
+    return (
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Apellido</TableCell>
+            <TableCell>Edad</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {usersList.map((user, index) => (
+            <TableRow key={index}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.lastName}</TableCell>
+              <TableCell>{user.age}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    )
 }
 
 export default App
