43d892e5fa8f16fc873be0766a65139980474ff2
Saul <saul_valenzuela_31@hotmail.com>
2025-03-27 09:25:45 -0700

    Cuarto commit: Adición de código original

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import './App.css'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"
 
 function App() {
+  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    if (count > 10) {
+      setCount(10)
+      alert("Llegaste al límite del contador")
+    }
+  },[count])
 
   const usersList = [
     {name:"Juan", lastName:"Mendoza", age:17}, 
@@ -33,6 +40,31 @@ function App() {
       </Table>
     </TableContainer>
     )
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
